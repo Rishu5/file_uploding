c0ad0deb6ed91fc58dfe3943895485af6cd0c29d
Hp <Hp@DESKTOP-GQ6PC08>
2023-04-27 08:19:00 +0530

    fild uploding

---
+++ File_Uploding_by_Boot/src/main/java/com/example/demo/crud/fileController.java
@@ -0,0 +1,55 @@
+package com.example.demo.crud;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import org.xml.sax.InputSource;
+
+import com.example.demo.crud.helper.Uplodefile;
+
+@RestController
+public class fileController {
+
+	@Autowired
+	public Uplodefile uplodefile;
+	
+	@PostMapping("/uplode-file")
+	public ResponseEntity<String> addFile(@RequestParam("file") MultipartFile file) {
+		
+		System.out.println(file.getName());
+		System.out.println(file.getOriginalFilename());
+		System.out.println(file.getSize());
+		System.out.println(file.getContentType());
+		
+		boolean isuploded=false;
+		try {
+			if(file.isEmpty()) {
+				
+				return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("file is empity");
+			}
+			if(!file.getContentType().equalsIgnoreCase("image/jpeg")) {
+				return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("only jpeg file are allowed");
+				
+			}
+			
+			//now file uplode code
+			isuploded = uplodefile.fileuUplode(file);
+	
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		if(isuploded) {
+			return ResponseEntity.ok("File uploded successfully");
+		}else{
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("file not uploded successfully");
+			
+		}
+		
+		
+	}
+}
---
+++ File_Uploding_by_Boot/src/main/java/com/example/demo/crud/helper/Uplodefile.java
@@ -0,0 +1,54 @@
+package com.example.demo.crud.helper;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+@Component
+public class Uplodefile {
+
+	public final String uplodedir = "D:\\S_T_S\\File_Uploding_by_Boot\\src\\main\\resources\\static\\image";
+
+	public boolean fileuUplode(MultipartFile file) {
+
+		boolean uploded = false;
+
+		try {
+			InputStream is = file.getInputStream();
+			byte data[] = new byte[is.available()];
+			is.read(data);
+
+			FileOutputStream fos = new FileOutputStream(uplodedir + File.separator + file.getOriginalFilename());
+			fos.write(data);
+			fos.flush();
+			fos.close();
+			
+			
+			//nio wala package for file uploding in one line
+			//Files.copy(file.getInputStream(), Paths.get(uplodedir+File.separator+file.getOriginalFilename()),StandardCopyOption.REPLACE_EXISTING);
+			
+			
+			
+			
+			
+			
+			uploded = true;
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return uploded;
+
+	}
+
+}
